fa6492cdd9e5a6eb4044b2881bde32a72b78df4c
Yihui Xie <xie@yihui.name>
2011-02-10 22:18:58 -0600

    add package introduction files

---
+++ R/MergeGUI-package.R
@@ -0,0 +1,9 @@
+##' A GUI for merging datasets in R.
+##'
+##' @name MergeGUI-package
+##' @aliases MergeGUI
+##' @docType package
+##' @exportPattern "^[^\\.]"
+##' @author Xiaoyue Cheng \email{xycheng@@iastate.edu}
+##'
+NULL
